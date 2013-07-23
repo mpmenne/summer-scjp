72b6a2b48312d5f910528248109dc41bba74c657
chip8874 <lemonbutter12321@yahoo.com>
2013-07-22 22:09:45 -0500

    Chip's notes

---
+++ Chapter3/src/com/scjp/prep/ChipNotes.java
@@ -0,0 +1,12 @@
+package com.scjp.prep;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Computer User
+ * Date: 7/22/13
+ * Time: 10:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChipNotes {
+    //test
+}
