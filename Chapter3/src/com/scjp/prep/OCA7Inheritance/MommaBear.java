3350c2a76739740329266c7b2a8d442297c4c19b
trenchwarfare <chris.m.zempel@gmail.com>
2013-07-31 13:11:58 -0500

    Added some inheritance practice

---
+++ Chapter3/src/com/scjp/prep/OCA7Inheritance/MommaBear.java
@@ -0,0 +1,11 @@
+package com.scjp.prep.OCA7Inheritance;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zempelc
+ * Date: 7/31/13
+ * Time: 1:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MommaBear {
+}
