9cf64060f64a474296a7e83a904f794f950badd7
trenchwarfare <chris.m.zempel@gmail.com>
2013-08-13 21:45:28 -0500

    LastDayPractice class added

---
+++ Chapter3/src/com/scjp/prep/LastDayPractice.java
@@ -0,0 +1,11 @@
+package com.scjp.prep;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zempelc
+ * Date: 8/13/13
+ * Time: 6:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LastDayPractice {
+}
